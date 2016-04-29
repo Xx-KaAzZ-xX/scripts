7cdbb63c8924aa3a933f16de837cc1c668e8afaa
Xx-KaAzZ-xX <Xx-KaAzZ-xX@users.noreply.github.com>
2016-04-29 12:10:54 +0200

    Update script_lvm.sh

--- script_lvm.sh
+++ script_lvm.sh
@@ -1,47 +1,39 @@
 #!/bin/bash
-
 ## Vérifier bien que lvm2 soit installé avant d'éxécuter le script ##
-
+echo -e "==== Disques disponibles ==== \n"
 fdisk -l | grep /dev/sd
-echo "Quel disque voulez-vous formater ? Exemple: /dev/sdb"
+echo -e "\n Quel disque voulez-vous formater ? Exemple: /dev/sdb"
 read cfdisk
 cfdisk $cfdisk
-
-echo "Actualisation des partitions... "
+echo -e "==== Actualisation des partitions ===== \n"
 fdisk -l | grep /dev/sd
-echo "Quel disque voulez-vous taguer ? Exemple /dev/sdc1"
+echo -e "\n Quel disque voulez-vous taguer ? Exemple /dev/sdc1"
 read pv_create
-echo "Création du Physical Volume..."
+echo -e "==== Création du PhysicalVolume ==== \n"
 pvcreate $pv_create
 
+echo "==== Volume Groups disponibles ===="
 pvdisplay | grep $pv_create
+vgdisplay |grep Name | awk '{print $3}'
 
-vgdisplay | grep Name | awk '{print $3}'
-
-echo "Quel Volume Group voulez-vous étendre ? Exemple: debian-vg "
+echo -e "\n Quel Volume Group voulez-vous étendre? Exemple: debian-vg"
 read vg
-echo "Ajouter le disque tagué au VG ? Exemple: /dev/sdb2"
+echo -e "Ajouter le disque tagué au VG ? Exemple: /dev/sdb2 \n"
 read disk_to_add
-
-#test=$(vgdisplay | grep Name)
-
-#if[test = vg]
+#test=$(vgdisplay | grep Name) if[test = vg]
 #	then
 		vgextend $vg $disk_to_add
 #	else
 #		echo "Vous avez entré un mauvais Volume Group "
 #fi
 lvscan
-
-echo " Veuillez saisir le chemin complet du Logical Volume Ex:/dev/debian-vg/home"
+echo -e " \n Veuillez saisir le chemin complet du Logical Volume Ex:/dev/debian-vg/home \n"
 read lv_path
 echo "De combien voulez-vous étendre" $vg "? Exemple : 2,5G - Ne pas utiliser de points !" 
 read lenght
 lvextend -L+$lenght $lv_path
-
 lvdisplay | grep 'LV Name\|LV Size'
-
-echo "Actualisation du système de fichiers... "
+echo -e "\n ==== Actualisation du système de fichiers... ==== "
 resize2fs $lv_path
 
 exit 0

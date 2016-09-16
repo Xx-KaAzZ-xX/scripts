677f29e97b433a50f4fb10a62eef7d099d1c93a7
Xx-KaAzZ-xX <Xx-KaAzZ-xX@users.noreply.github.com>
2016-09-16 10:52:21 +0200

    Update newEnv.sh

--- newEnv.sh
+++ newEnv.sh
@@ -415,6 +415,14 @@ fi
 
 ## Création BDD et USER
 
+echo "Choisir le format de DB"
+echo "1) utf8"
+echo "2) utf8mb4"
+read choice
+
+if [ $choice -eq 1 ]
+then
+
 echo "Password for SQL user: ${username}"
 read -s password
 echo "Confirmer le mot de passe SQL pour ${username}"
@@ -427,13 +435,37 @@ do
  echo "Confirmer le mot de passe SQL pour ${username}"
  read -s password2
 done
-Q1="CREATE DATABASE IF NOT EXISTS ${username};"
+Q1="CREATE DATABASE IF NOT EXISTS ${username} CHARACTER SET utf8;"
 Q2="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
 Q3="GRANT ALL PRIVILEGES ON ${username}.* TO '${username}'@'localhost';"
 Q4="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}${Q4}"
 
 mysql -u root -p -e "${SQL}"
+
+elif [ $choice -eq 2 ]
+then
+
+echo "Password for SQL user: ${username}"
+read -s password
+echo "Confirmer le mot de passe SQL pour ${username}"
+read -s password2
+while [ "${password}" != "${password2}" ]
+do
+ echo -e "Password don't match\n"
+ echo "Password for SQL user: ${username}"
+ read -s password
+ echo "Confirmer le mot de passe SQL pour ${username}"
+ read -s password2
+done
+Q1="CREATE DATABASE IF NOT EXISTS ${username} CHARACTER SET utf8mb4;"
+Q2="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
+Q3="GRANT ALL PRIVILEGES ON ${username}.* TO '${username}'@'localhost';"
+Q4="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+mysql -u root -p -e "${SQL}"
+
+fi
 
 ##Création espace FTP
 if [ ! -f /etc/pure-ftpd/db/mysql.conf ]

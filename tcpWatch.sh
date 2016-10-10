1787feacc8a2dbcfd34becffda4ff01eba74c303
Xx-KaAzZ-xX <Xx-KaAzZ-xX@users.noreply.github.com>
2016-10-10 22:38:53 +0200

    Update tcpWatch.sh

--- tcpWatch.sh
+++ tcpWatch.sh
@@ -3,22 +3,36 @@
 ## Description : Watch a connection and modify a file if a connection is 
 ## made. Then Inotify watchs the file and send an alert by email (cf incrontab -e)
 
+script="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 port="444"
 file="/tmp/connections.txt"
 file2="/tmp/test.txt"
 
-while true; do
+if [ -z "$1" ]
+then
+  echo "Usage : ${script} -p <port> or --port <port>"
+fi
 
-  lsof -i :${port} | grep ESTABLISHED > ${file}
-  before="$(wc -l ${file} | awk '{printf $1}')"
-  after="$(wc -l /tmp/connections.txt | awk '{printf $1}')"
-  
-  if [ ${after} > ${before} ]
-  then
-    echo "New connection" > ${file2}
-  fi
-sleep 5
+case "$1" in
+  -h | --help)
+    echo "Usage : ${script} -p <port> or --port <port>"
+    ;;
+  -p | --port)
+    ##Surveille les co entrantes , si une ligne est ajoutée , alors modif du file2
+    while true; do
+    lsof -i :${port} | grep ESTABLISHED > ${file}
+    before="$(wc -l ${file} | awk '{printf $1}')"
+    after="$(wc -l ${file} | awk '{printf $1}')"
+    
+    echo "before : $before"
+    echo "after : $after"
+      if [ "$after" -gt "$before" ]
+        then
+        echo "New connection" > ${file2}
+      fi
+    sleep 10
 done
-
+    ;;
+esac
 
 exit 0

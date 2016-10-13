774512cef8da7002f78464e8e2c2b7e988ac85d6
Xx-KaAzZ-xX <Xx-KaAzZ-xX@users.noreply.github.com>
2016-10-13 10:05:03 +0200

    Update changeInfo.sh

--- changeInfo.sh
+++ changeInfo.sh
@@ -4,9 +4,10 @@ echo "1) 164.132.82.32/27"
 echo "2) 37.59.237.160/27 "
 read choice
 
-case $choice in
-  1*)
-    echo "Renseigner l'adresse IP du serveur"
+if [ $choice -eq 1 ]
+then
+
+echo "Renseigner l'adresse IP du serveur"
     read ip_addr
 
     cat >> /etc/network/interfaces << _EOF_
@@ -22,9 +23,10 @@ case $choice in
         dns-search itserver.fr
 
 _EOF_
-;;
 
-  2*)
+elif [ $choice -eq 2 ]
+then
+
      echo "Renseigner l'adresse IP du serveur"
      read ip_addr
 
@@ -42,9 +44,14 @@ _EOF_
           dns-search itserver.fr
 
 _EOF_
-;;
 
-esac
+else
+
+  echo "Choisir une bonne option"
+  exit 1
+
+fi
+
 echo "Renseigner le nom de la machine"
 read name
 echo "${name}" > /etc/hostname
@@ -52,6 +59,16 @@ sed -i 's/template/'"${name}"'/g' /etc/hosts
 service hostname.sh
 echo "Renseigner le nouveau mot de passe root"
 read -s root_password
+
+while [ "${root_password}" != "${root_password2}" ]
+do
+   echo -e "Password don't match\n"
+   echo "Mot de passe UNIX pour root"
+   read -s root_password
+   echo "Confirmer le mot de passe"
+   read -s root_password2
+done
 echo "root:$root_password" | chpasswd
 
+echo "Think to ifup eth0"
 exit 0
